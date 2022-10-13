49ab53bd46e04991a34b7d9ebf7d28b580ea9c80
Mosu25 <mosu.avms@gmail.com>
2022-10-13 13:39:20 +0530

    Adding Get method using BDD style

---
+++ restAssured/src/test/java/restAssuredBDD/GetAllEmployees.java
@@ -0,0 +1,21 @@
+package restAssuredBDD;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class GetAllEmployees {
+
+	@Test
+	public void getAllEmployees() {
+		
+		RestAssured
+		.given()
+		.baseUri("http://localhost:3000")
+		.when()
+		.get("/employees")
+		.prettyPrint();
+		
+		
+	}
+}
